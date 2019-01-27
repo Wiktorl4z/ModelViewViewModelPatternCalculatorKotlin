b38f2bd039eb2784d68dc6fc6109384094782256
Wiktor Kalinowski <Wiktorl4z@users.noreply.github.com>
2019-01-27 21:46:26 +0100

    Added: DataBinding input and output for TextViews

--- app/src/main/java/pl/futuredev/modelviewviewmodelpatterncalculatorkotlin/viewmodel/CalculatorViewModel.kt
+++ app/src/main/java/pl/futuredev/modelviewviewmodelpatterncalculatorkotlin/viewmodel/CalculatorViewModel.kt
@@ -1,26 +1,54 @@
 package pl.futuredev.modelviewviewmodelpatterncalculatorkotlin.viewmodel
 
+import android.app.Application
 import android.content.ContentValues.TAG
+import android.databinding.BaseObservable
+import android.databinding.ObservableField
 import android.nfc.Tag
 import android.util.Log
+import kotlinx.android.synthetic.main.content_main.view.*
+import pl.futuredev.modelviewviewmodelpatterncalculatorkotlin.R
 import pl.futuredev.modelviewviewmodelpatterncalculatorkotlin.model.Calculator
 import pl.futuredev.modelviewviewmodelpatterncalculatorkotlin.model.TipCalculation
 
-class CalculatorViewModel(val calculator: Calculator = Calculator()) {
+class CalculatorViewModel(val app: Application, val calculator: Calculator = Calculator()) : BaseObservable() {
 
     var inputCheckAmount = ""
     var inputTipPercentage = ""
     var tipCalculation = TipCalculation()
 
-    fun calculateTip(){
+    var outputCheckAmount = ""
+    var outputTipAmount = ""
+    var outputTotalDollarAmount = ""
 
-        Log.d(TAG, "calculateTipInvoked")
+    init {
+        updateOutputs(TipCalculation())
+    }
+
+    private fun updateOutputs(tipCalculation: TipCalculation) {
+        outputCheckAmount = app.getString(R.string.dollar_amount, tipCalculation.checkAmount)
+        outputTipAmount = app.getString(R.string.dollar_amount, tipCalculation.tipAmount)
+        outputTotalDollarAmount = app.getString(R.string.dollar_amount, tipCalculation.grandTotal)
+    }
+
+    fun calculateTip() {
+
+        // Log.d(TAG, "calculateTipInvoked")
         val checkAmount = inputCheckAmount.toDoubleOrNull()
         val tipPct = inputTipPercentage.toIntOrNull()
 
-        if (checkAmount !=null && tipPct != null){
-            tipCalculation = calculator.calculatorTip(checkAmount, tipPct)
+        if (checkAmount != null && tipPct != null) {
+            //    Log.d(TAG, "CheckAmount: $checkAmount, TipPercentage: $tipPct")
+            updateOutputs(tipCalculation = calculator.calculatorTip(checkAmount, tipPct))
+            notifyChange()
+            clearInputs()
         }
 
+    }
+
+    private fun clearInputs() {
+        inputCheckAmount = "0.00"
+        inputTipPercentage = "0"
+        notifyChange()
     }
 }
--- app/src/test/java/pl/futuredev/modelviewviewmodelpatterncalculatorkotlin/viewmodel/CalculatorViewModelTest.kt
+++ app/src/test/java/pl/futuredev/modelviewviewmodelpatterncalculatorkotlin/viewmodel/CalculatorViewModelTest.kt
@@ -1,5 +1,6 @@
 package pl.futuredev.modelviewviewmodelpatterncalculatorkotlin.viewmodel
 
+import android.app.Application
 import junit.framework.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
@@ -7,6 +8,7 @@ import org.mockito.ArgumentMatchers
 import org.mockito.Mock
 import org.mockito.Mockito.*
 import org.mockito.MockitoAnnotations
+import pl.futuredev.modelviewviewmodelpatterncalculatorkotlin.R
 import pl.futuredev.modelviewviewmodelpatterncalculatorkotlin.model.Calculator
 import pl.futuredev.modelviewviewmodelpatterncalculatorkotlin.model.TipCalculation
 
@@ -18,10 +20,18 @@ class CalculatorViewModelTest {
     @Mock
     lateinit var mockCalculator: Calculator
 
+    @Mock
+    lateinit var application: Application
+
     @Before
     fun setup() {
         MockitoAnnotations.initMocks(this)
-        calculatorViewModel = CalculatorViewModel(mockCalculator)
+        stubResources(0.0,"$0.00")
+        calculatorViewModel = CalculatorViewModel(application,mockCalculator)
+    }
+
+    private fun stubResources(given: Double, returnStub: String){
+        `when` (application.getString(R.string.dollar_amount, given)).thenReturn(returnStub)
     }
 
     @Test
@@ -33,10 +43,15 @@ class CalculatorViewModelTest {
         val stub = TipCalculation(checkAmount = 10.00, tipAmount = 1.5, grandTotal = 11.5)
 
         `when`(mockCalculator.calculatorTip(10.00, 15)).thenReturn(stub)
+        stubResources(10.0,"$10.00")
+        stubResources(1.5,"$1.50")
+        stubResources(11.5,"$11.50")
 
         calculatorViewModel.calculateTip()
 
-        assertEquals(stub, calculatorViewModel.tipCalculation)
+        assertEquals("$10.00", calculatorViewModel.outputCheckAmount)
+        assertEquals("$1.50", calculatorViewModel.outputTipAmount)
+        assertEquals("$11.50", calculatorViewModel.outputTotalDollarAmount)
     }
 
     @Test
